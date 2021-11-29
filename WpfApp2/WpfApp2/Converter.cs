8411579b4fcfc11257956440b88ad243275d1358
Yuri Svechnikov <YSvechnikov@actsoft.com>
2021-11-29 16:49:40 +0600

    Converter WPF App

---
+++ WpfApp2/WpfApp2/Converter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp2
+{
+    class Converter
+    {
+        double usd, eur, rub;
+
+        public double Usd { get { return usd; } }
+        public double Eur { get { return eur; } }
+        public double Rub { get { return rub; } }
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public double BuyUSD(int summ)
+        {
+            double result = summ / usd;
+            return result;
+            //Console.WriteLine($"\nYou have bought {result} dollars");
+        }
+
+        public double SellUSD(int summ)
+        {
+            double result = usd * summ;
+            return result;
+            //Console.WriteLine($"You have sold {summ} dollars and got {result} soms");
+        }
+
+        public double BuyEUR(int summ)
+        {
+            double result = summ / eur;
+            return result;
+            //Console.WriteLine($"\nYou have bought {result} euros");
+        }
+
+        public double SellEUR(int summ)
+        {
+            double result = eur * summ;
+            return result;
+            //Console.WriteLine($"You have sold {summ} euros and got {result} soms");
+        }
+
+        public double BuyRUB(int summ)
+        {
+            double result = summ / rub;
+            return result;
+            //Console.WriteLine($"\nYou have bought {result} rubles");
+        }
+
+        public double SellRUB(int summ)
+        {
+            double result = rub * summ;
+            return result;
+            //Console.WriteLine($"You have sold {summ} rubles and got {result} soms");
+        }
+
+        public override string ToString()
+        {
+            return $"USD = {usd}\nEUR = {eur}\nRUB = {rub}";
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Converter myConverter = new Converter(84, 99, 2);
+
+        string kursValut = $"Текущий курс валют: 84";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Convert.ToString(myConverter), "Текущий курс валют");
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (soms.Text == string.Empty)
+                MessageBox.Show("Вы не ввели сумму!");
+            int amount = Convert.ToInt32(soms.Text);
+            myConverter.BuyUSD(amount);
+            MessageBox.Show(Convert.ToString(myConverter.BuyUSD(amount)), "Результат операции");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int amount = Convert.ToInt32(soms.Text);
+            myConverter.BuyEUR(amount);
+            MessageBox.Show(Convert.ToString(myConverter.BuyEUR(amount)), "Результат операции");
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            int amount = Convert.ToInt32(soms.Text);
+            myConverter.BuyRUB(amount);
+            MessageBox.Show(Convert.ToString(myConverter.BuyRUB(amount)), "Результат операции");
+        }
+
+        private void textBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            int amount = Convert.ToInt32(currency.Text);
+            myConverter.SellUSD(amount);
+            MessageBox.Show(Convert.ToString(myConverter.SellUSD(amount)), "Результат операции");
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            int amount = Convert.ToInt32(currency.Text);
+            myConverter.SellEUR(amount);
+            MessageBox.Show(Convert.ToString(myConverter.SellEUR(amount)), "Результат операции");
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            int amount = Convert.ToInt32(currency.Text);
+            myConverter.SellRUB(amount);
+            MessageBox.Show(Convert.ToString(myConverter.SellRUB(amount)), "Результат операции");
+        }
+    }
+}
